36b48e9a04b364e2475c969366b9fcd1e2b6a44a
purolink <purolink3@gmail.com>
2024-10-01 14:56:18 +0200

    Se modifica server.js para mostar versión de node aplicada

--- front-end/server.js
+++ front-end/server.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
 
-// Cambia la ruta estática para incluir el subdirectorio 'browser'
+app.use(cors());
+
+// Route to get node version
+app.get('/api/node-version', (req, res) => {
+  res.json({ version: process.version });
+});
+
 app.use(express.static(path.join(__dirname, 'dist/front-end/browser')));
 
-// Cambia la ruta del archivo index.html
+// to other route, get index.html from Angular
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/front-end/browser/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
