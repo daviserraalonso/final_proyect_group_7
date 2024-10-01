714eaa1d430ac292c529d45fb47b484e7922343e
purolink <purolink3@gmail.com>
2024-10-01 13:36:40 +0200

    Se modifica server.js para adaptar a render

--- front-end/server.js
+++ front-end/server.js
@@ -2,18 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// port proporcionate to render o localhost
-const PORT = process.env.PORT || 3000;
-
-// get statics files from angular
+// Asegúrate de que la ruta estática se ajuste a la nueva ubicación
 app.use(express.static(path.join(__dirname, 'dist/front-end')));
 
-// all routes should be get all index.html
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/front-end/index.html'));
 });
 
-// get post from render or localhost
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
