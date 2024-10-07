5704205130c8257135f9d6d408c8f47de78d72fc
David Serrano Alonso <daviserraalonso@gmail.com>
2024-10-07 08:47:53 +0200

    Se elimina proyecto angular y node para subir uno limpio

--- front-end/src/main.server.ts
+++
@@ -1,5 +0,0 @@
-import { platformServer } from '@angular/platform-server';
-import { AppModule } from './app/app.module';
-
-platformServer().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
