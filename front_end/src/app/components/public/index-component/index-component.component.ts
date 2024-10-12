5b5d2d7dd99c4e942847019774412d92196b64fd
David Serrano Alonso <daviserraalonso@gmail.com>
2024-10-12 20:04:16 +0200

    Renombro weak-up-component a index-component

---
+++ front_end/src/app/components/public/index-component/index-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'index',
+  standalone: true,
+  imports: [],
+  templateUrl: './index-component.component.html',
+  styleUrl: './index-component.component.css'
+})
+export class indexComponent {
+
+}
--- front_end/src/app/pages/public/home/home.component.ts
+++ front_end/src/app/pages/public/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { FavoriteTeachersComponent } from "../../../components/public/favorite-teachers/favorite-teachers.component";
 import { SearchTeachersComponent } from "../../../components/public/search-teachers/search-teachers.component";
-import { WakeUpCallComponent } from "../../../components/public/wake-up-call/wake-up-call.component";
+import { indexComponent } from "../../../components/public/index-component/index-component.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FavoriteTeachersComponent, SearchTeachersComponent, WakeUpCallComponent],
+  imports: [FavoriteTeachersComponent, SearchTeachersComponent, indexComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
