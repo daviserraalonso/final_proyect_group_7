d063a68721bae3dd94bf687c03cc562329c7fef2
Oscar rondon <oscarrondon77@gmail.com>
2024-10-12 14:10:54 +0200

    Maquetacion y agrego unos componentes
    component search-teachers, component wake-up-call, component favorite-teachers, component recovery-password

---
+++ front_end/src/app/components/public/wake-up-call/wake-up-call.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wake-up-call',
+  standalone: true,
+  imports: [],
+  templateUrl: './wake-up-call.component.html',
+  styleUrl: './wake-up-call.component.css'
+})
+export class WakeUpCallComponent {
+
+}
--- front_end/src/app/pages/public/home/home.component.ts
+++ front_end/src/app/pages/public/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FavoriteTeachersComponent } from "../../../components/public/favorite-teachers/favorite-teachers.component";
+import { SearchTeachersComponent } from "../../../components/public/search-teachers/search-teachers.component";
+import { WakeUpCallComponent } from "../../../components/public/wake-up-call/wake-up-call.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FavoriteTeachersComponent, SearchTeachersComponent, WakeUpCallComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
