e78e7e6f04568ab55a78a8286b8464c192922587
Oscar rondon <oscarrondon77@gmail.com>
2024-10-24 18:45:37 +0200

    DashBoardReady

--- front_end/src/app/components/dashboard/student/student-profile-component/student-profile-component.component.ts
+++ front_end/src/app/components/dashboard/student/student-profile-component/student-profile-component.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import {  Component } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 @Component({
   selector: 'app-student-profile-component',
