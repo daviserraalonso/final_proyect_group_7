761533f3b5481da438ee77fab98e9e47aebd8f16
Albert Martinez <161922205+Albertmmgit@users.noreply.github.com>
2024-10-28 07:09:30 +0100

    map

---
+++ front_end/src/app/components/public/map-component/datos.prueba.ts
@@ -0,0 +1,76 @@
+export const PROFESORES: any = [
+
+    {
+      id: 1,
+      nombre: "Juan",
+      precio: 29.99,
+      materia: 'lengua',
+      ubicacion: 'salou',
+      lat: 41.0768193,
+      lon: 1.1440411
+    },
+    {
+      id: 2,
+      nombre: "Oscar",
+      precio: 49.99,
+      materia: 'programación',
+      ubicacion: 'barcelona',
+      lat: 41.3828939,
+      lon: 2.1774322
+    },
+    {
+      id: 3,
+      nombre: "Albert",
+      precio: 19.99,
+      materia: 'ciencias',
+      ubicacion: 'zaragoza',
+      lat: 41.6521342,
+      lon: -0.8809428
+    },
+    {
+      id: 4,
+      nombre: "David",
+      precio: 39.99,
+      materia: 'matematicas',
+      ubicacion: 'tarragona',
+      lat: 41.1172364,
+      lon: 1.2546057
+    },
+    {
+      id: 5,
+      nombre: "Ivan",
+      precio: 59.99,
+      materia: 'lengua',
+      ubicacion: 'madrid',
+      lat: 40.4167047,
+      lon: -3.7035825
+    },
+    {
+      id: 6,
+      nombre: "Laura",
+      precio: 39.99,
+      materia: 'lengua',
+      ubicacion: 'reus',
+      lat: 41.1555564,
+      lon: 1.1076133
+    },
+    {
+      id: 7,
+      nombre: "Jose",
+      precio: 79.99,
+      materia: 'programación',
+      ubicacion: 'reus',
+      lat: 41.1555564,
+      lon: 1.1076133
+    },
+    {
+      id: 8,
+      nombre: "Antonio",
+      precio: 29.99,
+      materia: 'lengua',
+      ubicacion: 'valencia',
+      lat: 39.4697065,
+      lon: -0.3763353
+    },
+    
+  ]
--- front_end/src/app/components/public/map-component/map-component.component.ts
+++ front_end/src/app/components/public/map-component/map-component.component.ts
@@ -1,12 +1,111 @@
-import { Component } from '@angular/core';
+import { Component, inject, PLATFORM_ID, signal } from '@angular/core';
+import { GoogleMap, GoogleMapsModule, MapInfoWindow, MapMarker } from '@angular/google-maps';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import { isPlatformBrowser } from '@angular/common';
+import {MatRadioModule} from '@angular/material/radio';
+import {FormsModule} from '@angular/forms';
+import { EventEmitter } from 'stream';
+import { PROFESORES } from './datos.prueba';
+
 
 @Component({
   selector: 'app-map-component',
   standalone: true,
-  imports: [],
+  imports: [GoogleMap, GoogleMapsModule, MapMarker, MatCardModule, MatIconModule, MatRadioModule, FormsModule],
   templateUrl: './map-component.component.html',
   styleUrl: './map-component.component.css'
 })
 export class MapComponentComponent {
 
+  teachers: any = PROFESORES
+  filterteacher: any = this.teachers
+
+
+  myPosition = signal<google.maps.LatLngLiteral>({lat: 0, lng: 0});
+  position = signal<google.maps.LatLngLiteral>({lat: 0, lng: 0});
+  myLat: any = ""
+  myLng: any = ""
+  mapZoom = signal<number>(15)
+  plataformId = inject(PLATFORM_ID)
+  openWindow: MapInfoWindow | null = null
+
+  mapOptins = signal({
+    zoom: this.mapZoom()
+  })
+labelPosition: any;
+
+
+prueba(event: any) {
+  const filter = event.target.value
+  console.log(event.target.value)
+  this.filterteacher = this.teachers.filter((teacher: { materia: any; }) => teacher.materia == filter)
 }
+
+
+  createIcon() {
+    if (isPlatformBrowser(this.plataformId)) {
+    return {
+      url: 'assets/icono.png' ,
+      scaledSize: new google.maps.Size(50, 40)
+    }
+  } return null
+}
+  
+
+   ngOnInit() {
+    this.myLocation()
+
+  }
+
+
+  myLocation() {
+    if (isPlatformBrowser(this.plataformId)) {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.myLat = position.coords.latitude
+      this.myLng = position.coords.longitude
+      this.myPosition.set({lat: this.myLat, lng: this.myLng})
+      this.position.set({lat: this.myLat, lng: this.myLng})
+      this.mapOptins.set({
+        zoom: this.mapZoom()
+      })
+      
+    })
+  }
+  }
+
+  teacherLocation(lat: number, lng: number) {
+    this.position.set({lat: lat, lng: lng})
+  }
+
+
+
+  addMarkers(lat: any, lng: any) {
+    if (isPlatformBrowser(this.plataformId)) {
+        return new google.maps.LatLng(lat, lng)
+  } return lat
+}
+
+openInfoWindow(marker: MapMarker, infoWindow: MapInfoWindow) {
+  if(this.openWindow) {
+    this.openWindow.close()
+  }
+  infoWindow.open(marker)
+  this.openWindow = infoWindow
+
+}
+
+
+
+  // getDistance(lat: any, lon: any) {
+  
+  //   const url = `https://maps.googleapis.com/maps/api/distancematrix/json
+  //   ?destinations=${lat}%2C${lon}
+  //   &origins=${this.myLat}%2C${this.myLon}
+  //   &key=${this.key}`
+  // }
+
+}
+
+
