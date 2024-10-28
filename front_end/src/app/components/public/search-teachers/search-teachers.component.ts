ddb595631125968d4bcd4be8fb56e7a794cb1d11
Albert Martinez <161922205+Albertmmgit@users.noreply.github.com>
2024-10-28 07:11:16 +0100

    prueba

--- front_end/src/app/components/public/search-teachers/search-teachers.component.ts
+++ front_end/src/app/components/public/search-teachers/search-teachers.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { MapComponentComponent } from '../map-component/map-component.component';
 
 @Component({
   selector: 'app-search-teachers',
   standalone: true,
-  imports: [RouterOutlet,FormsModule],
+  imports: [RouterOutlet,FormsModule, MapComponentComponent],
   templateUrl: './search-teachers.component.html',
   styleUrl: './search-teachers.component.css'
 })
