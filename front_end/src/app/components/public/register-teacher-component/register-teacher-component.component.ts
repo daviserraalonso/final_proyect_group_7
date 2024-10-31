8e0e82c900a1ef0ebb9d63c4d316ad4ee13ea871
Albert Martinez <161922205+Albertmmgit@users.noreply.github.com>
2024-10-31 07:34:07 +0100

    add city field to the form and autocomplet function

--- front_end/src/app/components/public/register-teacher-component/register-teacher-component.component.ts
+++ front_end/src/app/components/public/register-teacher-component/register-teacher-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, ElementRef, inject, signal, ViewChild } from '@angular/core';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -8,23 +8,28 @@ import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validator
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserServiceService } from '../../../service/user-service.service';
 import { UserAttributes } from '../../../interfaces/userAttributes';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 
 
 @Component({
   selector: 'app-register-teacher-component',
   standalone: true,
-  imports: [MatSelectModule, MatInputModule, MatFormFieldModule, MatIconModule, MatButtonModule, ReactiveFormsModule],
+  imports: [MatSelectModule, MatInputModule, MatFormFieldModule, MatIconModule, MatButtonModule, ReactiveFormsModule, GoogleMapsModule],
   templateUrl: './register-teacher-component.component.html',
   styleUrl: './register-teacher-component.component.css'
 })
 export class RegisterTeacherComponentComponent {
+
+  @ViewChild('cityInput') cityInput!: ElementRef
+
   router = inject(Router);
   activateRoute = inject(ActivatedRoute);
   userServices = inject(UserServiceService)
   headerForm: string = 'Registrarse como profesor' // header of form
   textButton: string = 'Enviar' // text of button submit
   registerTeacher: FormGroup;
+  selectedPlace: any;
 
 
   // initialize form
@@ -42,6 +47,16 @@ export class RegisterTeacherComponentComponent {
       phone: new FormControl(null, [
         Validators.required
       ]),
+      city: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      lat: new FormControl(null, [
+        Validators.required
+      ]),
+      lng: new FormControl(null, [
+        Validators.required
+      ]),
       password: new FormControl(null, [
         Validators.required,
         Validators.minLength(8),
@@ -55,6 +70,12 @@ export class RegisterTeacherComponentComponent {
       ])
     }, [this.checkPassword])
   }
+
+  ngAfterViewInit() {
+    this.autocomplet()
+  
+  }
+  
 
   // recovery user data to update
 ngOnInit() {
@@ -76,6 +97,16 @@ ngOnInit() {
       Validators.pattern(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)
     ]),
     phone: new FormControl(user.phone, [
+      Validators.required
+    ]),
+    city: new FormControl(null, [ // city ​​field missing in interface
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    lat: new FormControl(user.lat, [
+      Validators.required
+    ]),
+    lng: new FormControl(user.lng, [
       Validators.required
     ]),
     password: new FormControl(user.password, [
@@ -129,6 +160,7 @@ clickEvent2(event: MouseEvent) {
 
 // sending form data to the service
 async getdataForm() {
+  console.log(this.registerTeacher.value)
   if (this.registerTeacher.value._id) {
     try {
       const user: UserAttributes = await this.userServices.update(this.registerTeacher.value)     
@@ -148,6 +180,23 @@ async getdataForm() {
     } catch (error) {   
     }
   }
+  }
+
+
+    // Create a autocomplet in a Input City
+  autocomplet() {
+    const autocomplete = new google.maps.places.Autocomplete(this.cityInput.nativeElement);
+    autocomplete.setTypes(['(cities)']); // to show only cities
+    autocomplete.addListener('place_changed', () => {
+      this.selectedPlace = autocomplete.getPlace();
+      //add data to the form
+      this.registerTeacher.patchValue({
+        lat: this.selectedPlace.geometry.location.lat(),
+        lng: this.selectedPlace.geometry.location.lng(),
+        city: this.selectedPlace.name
+      }) 
+    });
+
   }
 }
 
