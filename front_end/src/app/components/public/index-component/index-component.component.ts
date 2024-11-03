803568aa9cf265549d23356a9873207294c2dca0
david <david-moreno-@hotmail.es>
2024-11-03 21:08:49 +0100

    retocar el index

--- front_end/src/app/components/public/index-component/index-component.component.ts
+++ front_end/src/app/components/public/index-component/index-component.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'index',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './index-component.component.html',
   styleUrl: './index-component.component.css'
 })
