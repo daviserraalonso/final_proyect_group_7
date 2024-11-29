e7d57a9433ca14b19c59d4d0e34161a09a2577dd
david <david-moreno-@hotmail.es>
2024-11-29 18:30:21 +0100

    css

--- front_end/src/app/components/public/how-work-component/how-work-component.component.ts
+++ front_end/src/app/components/public/how-work-component/how-work-component.component.ts
@@ -9,10 +9,5 @@ import { Component } from '@angular/core';
 })
 export class HowWorkComponentComponent {
 
-  cards = [
-    { image: 'assets/professor1.jpeg', title: 'Clase con el profesor más cool' },
-    { image: 'assets/professor2.jpg', title: 'Estudiantes comprometidos' },
-    { image: 'assets/professor3.jpeg', title: 'Superprof destacado' },
-    { image: 'assets/professor1.jpeg', title: 'Innovación en la educación' },
-  ];
+
 }
