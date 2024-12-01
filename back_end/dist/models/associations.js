706162ba5cf46618db6aa10d1685d46d313ac7d4
David Serrano <daaviserraalonso@gmail.com>
2024-12-01 08:55:16 +0100

    New funcionalillty puntuation teacher and course

--- back_end/dist/models/associations.js
+++ back_end/dist/models/associations.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = setupAssociations;
 const user_1 = __importDefault(require("./user"));
 const UserDetails_1 = __importDefault(require("./UserDetails"));
 const Course_1 = __importDefault(require("./Course"));
@@ -240,4 +241,3 @@ function setupAssociations() {
         as: 'averageCourse'
     });
 }
-exports.default = setupAssociations;
