66d719e1a3f740ad93e8b9437feac216577bddd5
david <david-moreno-@hotmail.es>
2024-12-04 01:05:57 +0100

    s

--- back_end/dist/routes/scoreRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const ScoreController_1 = require("../controllers/ScoreController");
-const router = (0, express_1.Router)();
-router.get('/', ScoreController_1.getScore);
-router.post('/', ScoreController_1.insertScore);
-module.exports = router;
