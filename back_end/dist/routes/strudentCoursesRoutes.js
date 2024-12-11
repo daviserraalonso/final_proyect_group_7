3f908648134c23281db1ff56459737c98c1dc09d
david <david-moreno-@hotmail.es>
2024-12-11 17:57:35 +0100

    com

--- back_end/dist/routes/strudentCoursesRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const StudentController_1 = require("../controllers/StudentController");
-const router = (0, express_1.Router)();
-// route specific to user
-router.get('/:userId', StudentController_1.getStudentCourses);
-router.post('/inscription', StudentController_1.inscription);
-module.exports = router;
