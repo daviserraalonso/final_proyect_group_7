7ae770ad449be1d4989ad281235a87ec0b73e7e1
daviserraalonsom <daviserraalonso@gmail.com>
2024-12-11 08:17:14 +0100

    Se hacen modificaciones para arreglar modelo user a User

--- back_end/dist/models/associations.js
+++ back_end/dist/models/associations.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = setupAssociations;
 const UserDetails_1 = __importDefault(require("./UserDetails"));
 const Course_1 = __importDefault(require("./Course"));
 const StudentCourse_1 = __importDefault(require("./StudentCourse"));
@@ -300,4 +301,3 @@ function setupAssociations() {
         onUpdate: 'CASCADE',
     });
 }
-exports.default = setupAssociations;
